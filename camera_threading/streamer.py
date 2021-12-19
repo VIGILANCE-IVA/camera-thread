daa4f38e879de83eb58a09cd84144a2d5bc35ea7
paulgrammer <paulmugaya@live.com>
2021-12-19 12:18:07 +0300

    added resolution setter

--- camera_threading/streamer.py
+++ camera_threading/streamer.py
@@ -13,28 +13,59 @@ class Capture(threading.Thread):
         self.__flag.set()  # Set to True
         self.__running = threading.Event()  # Used to stop the thread identification
         self.__running.set()  # Set running to True
-        self.__emitter = EventEmitter()
-        self.__delay = delay
-        self.__source = source
+        self.emitter = EventEmitter()
+        self.delay = delay
+        self.source = source
+        self.resolution = {
+            '3': 640,
+            '4': 480
+        }
 
     def on(self, event, f):
         # run all events
-        self.__emitter.on(event, f)
+        self.emitter.on(event, f)
 
     def emit(self, event, *args, **kwargs):
-        self.__emitter.emit(event, *args, **kwargs)
+        self.emitter.emit(event, *args, **kwargs)
+
+    def set(self, key, value):
+        self.resolution[str(key)] = value
+
+    def make_1080p(cap):
+        cap.set(3, 1920)
+        cap.set(4, 1080)
+        return cap
+
+    def make_720p(cap):
+        cap.set(3, 1280)
+        cap.set(4, 720)
+        return cap
+
+    def make_480p(cap):
+        cap.set(3, 640)
+        cap.set(4, 480)
+        return cap
+
+    def change_res(cap, width, height):
+        cap.set(3, width)
+        cap.set(4, height)
+        return cap
 
     def run(self):
-        if self.__source.isdigit():
-            self.__source = int(self.__source)
+        if str(self.source).isdigit():
+            self.source = int(self.source)
 
         # capture stream
-        capture = cv.VideoCapture(self.__source)
+        capture = cv.VideoCapture(self.source)
+
+        # resolution
+        capture.set(3, self.resolution['3'])
+        capture.set(4, self.resolution['4'])
 
         while self.__running.isSet():
             self.__flag.wait()  # return immediately when it is True, block until the internal flag is True when it is False
             ret, frame = capture.read()
-            if self.__source == 0:
+            if self.source == 0:
                 frame = cv.flip(frame, 1)  # flip if webcam
 
             # In case the image is not read properly
@@ -47,7 +78,7 @@ class Capture(threading.Thread):
                 exit = True
                 break
 
-            time.sleep(int(self.__delay))  # By time.sleep(1) you repeat the loop every 1 sec.
+            time.sleep(int(self.delay))  # By time.sleep(1) you repeat the loop every 1 sec.
 
         capture.release()
         cv.destroyAllWindows()
